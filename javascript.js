785c33661ef737b920f22022e02002bc9f2019a8
EdgarasMon <edgarasxxx@gmail.com>
2021-11-12 10:34:24 +0200

    Added functionality

--- javascript.js
+++ javascript.js
@@ -1,25 +1,10 @@
 document.getElementById('add').onclick = function changeContent(){
     var product = document.getElementById('first').value;
     var productQty = document.getElementById('second').value;
-    //console.log(product);
-    //console.log(productQty);
-    //localStorage.setItem(product, productQty);
-    
-    //container2.innerText += "New list :" + "\n";
-    //container2.innerText += localStorage.getItem();
-    //container2.innerText += " " + productQty + "X";
-    //console.log(localStorage);
-    for (let i = 0; i < localStorage.length; i++){
-        const key = localStorage.key(i);
-        const value = localStorage.getItem(key);        
-        container2.innerText += key + "\t";
-        container2.innerText += value + "X";
-        container2.innerText += "\n";
-    }
-    document.getElementById('first').value='';
-    document.getElementById('second').value='';
-    document.reload();
 
+    container2.innerText += product;
+    container2.innerText += " " + productQty + "X" + "\n";    
+    console.log(localStorage);  
 }
 
 
